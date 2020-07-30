d15b16c5f8d70cc7549945961da4b2311a6ae0c1
MaxKlaxxMiner <max@tetrisk.de>
2020-07-30 17:44:04 +0200

    initial: ScreenShoot-Tool: "scrshtr"

---
+++ scrshtr/Program.cs
@@ -0,0 +1,25 @@
+﻿#region # using *.*
+// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace scrshtr
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine();
+      Console.WriteLine("  * * * * * * * * * * * *");
+      Console.WriteLine("  *                     *");
+      Console.WriteLine("  *  Hello Screenshot!  *");
+      Console.WriteLine("  *                     *");
+      Console.WriteLine("  * * * * * * * * * * * *");
+      Console.WriteLine();
+    }
+  }
+}
